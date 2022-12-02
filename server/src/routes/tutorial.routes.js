16b747dd08a673045d44cd785bd0dbc27b5d7a6e
Leonardo <leopesi@yahoo.com.br>
2022-12-02 17:46:35 -0300

    Refact routes/tutorial.routes.js

--- server/src/routes/tutorial.routes.js
+++ server/src/routes/tutorial.routes.js
@@ -3,7 +3,7 @@ const postController = require('../controllers/tutorial.controller')
 
 //Rotas
 routes.post('/tutorials', postController.create);
-routes.get('/tutorials', postController.find);
+routes.get('/tutorials', postController.findAll);
 routes.get('/tutorials/:id', postController.findById);
 routes.put('/tutorials/:id', postController.update);
 routes.delete('/tutorials/:id', postController.deleteById);
