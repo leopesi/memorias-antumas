3139dc1a310eb9890d4a99b608dc9173d6eb572b
Leonardo <leopesi@yahoo.com.br>
2022-12-02 17:53:51 -0300

    Refact models/tutorial.model.js

--- server/src/models/tutorial.model.js
+++ server/src/models/tutorial.model.js
@@ -1,28 +1,19 @@
-const db = require("../db/db");
-
-const Sequelize = require('sequelize');
-
-const Tutorial = db.define('tutorial', {
-    id: {
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true
-    },
-    title: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-    description: {
-        type: Sequelize.STRING,
-        allowNull: false,
-    },
-})
-//Criar a tabela caso ainda não esteja criada
-//User.sync();
-
-
-//Verifica e faz alteração na tabela caso exista alguma
-//Tutorial.sync({alter: true})
-
-module.exports = Tutorial;
+module.exports = (sequelize, Sequelize) => {
+    const Tutorial = sequelize.define('tutorial', {
+        id: {
+            type: Sequelize.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        title: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: Sequelize.STRING,
+            allowNull: false,
+        },
+    })
+    return Tutorial
+}
