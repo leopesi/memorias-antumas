d974af1bea3e57cb974db09c23c3291da1759769
Leonardo <leopesi@yahoo.com.br>
2022-12-02 17:58:25 -0300

    Added routes/comment.routes.js

---
+++ server/src/routes/comment.routes.js
@@ -0,0 +1,13 @@
+const routes = require('express').Router();
+const postComment = require('../controllers/comment.controller')
+
+//Rotas
+
+routes.post('/comments', postComment.create);
+routes.get('/comments', postComment.find);
+routes.get('/comments/:id', postComment.findById);
+routes.put('/comments/:id', postComment.update);
+routes.delete('/comments/:id', postComment.deleteById)
+
+//Modulo
+module.exports = routes;
