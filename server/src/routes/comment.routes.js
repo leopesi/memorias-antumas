b5facf874476b08a0bd043f73f162cce6aaa7b15
Leonardo <leopesi@yahoo.com.br>
2022-12-02 18:21:43 -0300

    Refact oneToMany comment.routes.js

--- server/src/routes/comment.routes.js
+++ server/src/routes/comment.routes.js
@@ -3,7 +3,7 @@ const postComment = require('../controllers/comment.controller')
 
 //Rotas
 
-routes.post('/comments', postComment.create);
+routes.post('/comments/:id', postComment.create);
 routes.get('/comments', postComment.find);
 routes.get('/comments/:id', postComment.findById);
 routes.put('/comments/:id', postComment.update);
